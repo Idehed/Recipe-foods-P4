6c696175d03475e6ac22f70e3ac6933e8278a029
Idehed anton <anton.idehed@telia.com>
2024-04-11 08:27:10 +0000

    adding recipes to admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Recipe
 
-# Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display= (
+        'title',
+        'meal_types',
+        'calories',
+        'instructions',
+        'ingredients',
+        'image'
+    )
+    list_filter = ('meal_type',)
