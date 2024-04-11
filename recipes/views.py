00a51927b3a1be50b9281d4c8f3e9a055eedd710
Idehed anton <anton.idehed@telia.com>
2024-04-11 17:24:05 +0000

    creating html for recipes and forms.py and urls.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+from .models import Recipe
+
+class AddRecipe(CreateView):
+    """
+    Add recipe view
+    """
+    template_name = 'recipes/add_recipe.html'
+    model = Recipe
+    sucess_url = '/recipes/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddRecipe, self).form_valid(form)
