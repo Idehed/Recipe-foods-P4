253d47714608662f33642dc40fa97523a10a7091
Idehed anton <anton.idehed@telia.com>
2024-04-15 16:13:45 +0000

    added a class in views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, UpdateView
+from django.contrib.auth.mixin import UserPassesTestMixin, LoginRequiredMixin
+
 from .models import Profile
+from .forms import ProfileForm
 
 class Profiles(TemplateView):
     """
@@ -14,3 +17,18 @@ class Profiles(TemplateView):
         }
 
         return context
+
+
+class EditProfile(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """
+    Edit profile 
+    """
+    form_class = ProfileForm
+    model = Profile
+    
+    def form_valid(self, form):
+        self.success_url = f'/profile/view/{self.kwargs['pk']}'
+        return super().form_valid(form)
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
