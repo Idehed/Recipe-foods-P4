581047e1f95904ba2ff2a52788069c8da3c39b39
Idehed anton <anton.idehed@telia.com>
2024-04-15 15:11:42 +0000

    created a class in views.py and created urls.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Profile
 
-# Create your views here.
+class Profiles(TemplateView):
+    """
+    User Profile View
+    """
+    template_name = "profiles/profile.html"
+
+    def get_context_date(self, **kwargs):
+        profile = Profile.object.get(user=self.kwargs["pk"])
+        context = {
+            'profile': profile
+        }
+
+        return context
