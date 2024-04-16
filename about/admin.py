9fa1760c910f3623586e767f2b86f388a25ce25c
Idehed anton <anton.idehed@telia.com>
2024-04-16 18:45:43 +0000

    register the model in admin.py and created function and html code

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .model import About
 
-# Register your models here.
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
+
